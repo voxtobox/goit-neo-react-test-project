b5f5d0fb7f41fb846451ca3b70d675096548303e
Volodymyr Antoniuk <voxtobox@gmail.com>
2025-01-14 21:54:30 -0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AppRoutes from './routes/AppRoutes';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
