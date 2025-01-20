14dafb3b25f6f482cfc12c82e2577116bb2804fe
Volodymyr Antoniuk <voxtobox@gmail.com>
2025-01-19 16:52:41 -0800

    fix home page container

--- src/pages/PageHome/PageHome.jsx
+++ src/pages/PageHome/PageHome.jsx
@@ -4,7 +4,7 @@ import css from './PageHome.module.css';
 
 export default function PageHome() {
   return (
-    <main>
+    <main className="container">
       <section className={css.hero}>
         <div className={css.container}>
           <h1 className={css.heroTitle}>Campers of your dreams</h1>
